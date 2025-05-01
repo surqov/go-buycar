4f5544d00b1d410f2e9947d8fdacddff7a103716
Gleb Surkov <writeln7@gmail.com>
2025-05-01 16:33:53 +0200

    [+] Changed car.go \

--- internal/models/car.go
+++ internal/models/car.go
@@ -1,79 +1,140 @@
 package models
 
-type BodyType int
-type StateType int
-type WheelSide bool
+type (
+	BodyType          string
+	StateType         int
+	WheelSide         bool
+	SteeringWheelSide bool
+	DriveType         string
+	AddonsList        []string
+	EquipmentList     []string
+	OptionsList       []string
+	SeatMaterial      string
+)
+
+type Car struct {
+	ID                 uint64
+	Images             []string
+	Status             bool
+	State              StateType
+	VendorID           int
+	ModelName          string
+	Generation         int
+	Year               int
+	OwnersCount        int
+	DriveType          DriveType
+	WheelsSize         WheelsInfo
+	InteriorMaterial   InteriorMaterial
+	MileageKm          int
+	DoorsNumber        int
+	SeatsNumber        int
+	BodyType           BodyType
+	Color              Color
+	City               string
+	EngineType         EngineType
+	Engine             EngineInfo
+	Equipment          EquipmentList
+	Transmission       TransmissionInfo
+	SteeringWheel      SteeringWheelSide
+	Condition          string
+	PTS                string
+	CustomsCleared     bool
+	ExchangePossible   bool
+	VIN                string
+	TestDriveAvailable bool
+	Dimensions         Dimensions
+	DrivingParams      DrivingParams
+	Options            OptionsList
+	Addons             *AddonsList
+}
 
 type Dimensions struct {
-	WidthCm     int // ширина
-	LengthCm    int // длина
-	HeightCm    int // высота
-	ClearanceCm int // клиренс
-	WheelbaseCm int // колёсная база
+	WidthCm      int // ширина
+	LengthCm     int // длина
+	HeightCm     int // высота
+	ClearanceCm  int // клиренс
+	WheelbaseCm  int // колёсная база
 	TrackWidthCm int // колея
-	MassKg      int  // масса
+	MassKg       int // масса
 }
 
 type DrivingParams struct {
-	MaxSpeedKph      int     // Максимальная скорость
-	AccelerationTo100 float64 // Разгон до 100 км/ч
-	FuelConsumptionCity float64
+	MaxSpeedKph            int     // Максимальная скорость
+	AccelerationTo100      float64 // Разгон до 100 км/ч
+	FuelConsumptionCity    float64
 	FuelConsumptionHighway float64
-	FuelConsumptionMixed float64
-	FuelType          string
-	EcoClass          string
-	CO2Emissions      int // грамм на км
+	FuelConsumptionMixed   float64
+	FuelType               string
+	EcoClass               string
+	CO2Emissions           int // грамм на км
 }
 
-// Можно будет вынести отдельно
-// type Wheels struct {}
-// EquipmentInfo?
-// type SuspensionAndBrakes struct {}
+type WheelSize struct {
+	Width    float64 // в дюймах, например 8.5
+	Diameter int     // диаметр в дюймах, например 19
+	ET       int     // вылет диска (ET), например 32
+}
+
+type TireSize struct {
+	Width_mm      int // мм (e.g. 255)
+	AspectRatio   int // % (e.g. 45)
+	Diameter_inch int // дюймы (e.g. 18)
+}
+
+type BoltPattern struct {
+	PCD        string // пример: "5x112"
+	CenterBore string // пример: "66.6" (мм)
+}
+
+type WheelsInfo struct {
+	TireSizes   []TireSize
+	WheelSizes  []WheesSize
+	BoltPattern BoltPattern
+}
+
+// TODO::
+type SuspensionAndBrakes struct{}
 
 const (
-	Liftback BodyType = iota
-	Sedan
-	SUV
-	Hatchback
+	Sedan     BodyType = "Sedan"
+	Hatchback BodyType = "Hatchback"
+	Liftback  BodyType = "Liftback"
+	SUV       BodyType = "SUV"
+	Wagon     BodyType = "Wagon"
+	Coupe     BodyType = "Coupe"
+	Minivan   BodyType = "Minivan"
+	Pickup    BodyType = "Pickup"
+	Limousine BodyType = "Limousine"
+	Van       BodyType = "Van"
+	Cabriolet BodyType = "Cabriolet"
 )
 
 const (
 	New StateType = iota
 	Used
+	NeedRepair
 )
 
 const (
-	Right WheelSide = true
-	Left  WheelSide = false
+	Left  WheelSide = true
+	Right WheelSide = false
 )
 
-type Car struct {
-	ID                 uint64
-	Status             bool
-	State              StateType
-	VendorID           int
-	ModelName          string
-	Year               int
-	Generation         int
-	Trim               string
-	DriveType          LocalizedString
-	WheelSize          Wheels 
-	OwnersCount        int
-	MileageKm          int
-	BodyType           BodyType
-	Color              Color
-	City               string
-	Engine             EngineInfo
-	Equipment          EquipmentInfo
-	Transmission       LocalizedString
-	SteeringWheel      WheelSide
-	Condition          LocalizedString
-	PTS                LocalizedString
-	CustomsCleared     bool
-	ExchangePossible   bool
-	VIN                string
-	TestDriveAvailable bool
-	Addons             []string
-	Dimensions         Dimensions
-	DrivingParams      DrivingParams
-}
+const (
+	DriveTypeFWD DriveType = "FWD" // передний привод (Front-Wheel Drive)
+	DriveTypeRWD DriveType = "RWD" // задний привод (Rear-Wheel Drive)
+	DriveTypeAWD DriveType = "AWD" // полный привод (All-Wheel Drive)
+	DriveType4WD DriveType = "4WD" // жёстко подключаемый полный привод (Four-Wheel Drive)
+)
+
+// TODO::
+type TransmissionInfo struct{}
+
+// TODO::
+const (
+	Leather    SeatMaterial = "leather"
+	Alcantara  SeatMaterial = "alcantara"
+	Fabric     SeatMaterial = "fabric"
+	EcoLeather SeatMaterial = "eco_leather"
+	Vinyl      SeatMaterial = "vinyl"
+)
